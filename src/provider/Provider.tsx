851915f75c92b8173984452ccb8d4c7cc0295a7b
KwonSeon <ksun2128@pukyong.ac.kr>
2024-10-03 18:46:45 +0900

    Feat: session provider
    - Add session provider
    session을 불러올 수 있게 provider를 추가했습니다.

--- src/provider/Provider.tsx
+++ src/provider/Provider.tsx
@@ -1,7 +1,11 @@
 'use client';
 
 import { NextUIProvider } from '@nextui-org/react';
+import { SessionProvider } from 'next-auth/react';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  return;
+  <SessionProvider>
+    <NextUIProvider>{children}</NextUIProvider>
+  </SessionProvider>;
 }
